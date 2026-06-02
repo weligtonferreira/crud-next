f396d92105bb23d48faa0c6b6f535554e1fcfe10
GabPaola <gabrielepc02@gmail.com>
2026-06-01 21:05:38 -0300

    feat: add RegisterPage

---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <p>This is the registration page.</p>
+        </div>
+    );
+
+}
