96dde621c41084abdc34ef80d0fba96ab9728254
Weligton <weligton0ferreira@gmail.com>
2026-06-01 22:32:36 -0300

    feat(api): add token to authorization headers

--- app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function RegisterPage() {
-    return (
-        <div>
-            <h1>Register Page</h1>
-            <p>This is the registration page.</p>
-        </div>
-    );
-
-}
